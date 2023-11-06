704f0edfd53a4ec372642ffdbe897a6073930dcf
a1749310 <highnessatharva@gmail.com>
2023-11-06 12:23:14 +0530

    Add Alumni and FETS components

--- src/components/Alumni/AlumniPage.js
+++ src/components/Alumni/AlumniPage.js
@@ -1,13 +1,66 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Image } from "react-bootstrap";
-import "./Alumni.css";
+import { Container, Row, Col, Image, Card } from "react-bootstrap";
+import "./Alumni.css"; // Your custom CSS file
 
 class AlumniPage extends Component {
   render() {
     return (
-      <>
-        <h1>Alumni Testimonials Page</h1>
-      </>
+      <Container className="alumni-container">
+        <h1 className="alumni-heading">Alumni Testimonials</h1>
+        <Row>
+          <Col lg={4} md={6} sm={12} className="alumni-card">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://picsum.photos/130/130?image=1027"
+                alt="Alumni 1"
+                className="alumni-image"
+              />
+              <Card.Body>
+                <Card.Title>John Doe</Card.Title>
+                <Card.Text>
+                  "I had a great experience at our university. The faculty and
+                  programs are top-notch."
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={4} md={6} sm={12} className="alumni-card">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://picsum.photos/130/130?image=1028"
+                alt="Alumni 2"
+                className="alumni-image"
+              />
+              <Card.Body>
+                <Card.Title>Jane Smith</Card.Title>
+                <Card.Text>
+                  "The education I received here was invaluable. It prepared me
+                  for my career."
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={4} md={6} sm={12} className="alumni-card">
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://picsum.photos/130/130?image=1029"
+                alt="Alumni 3"
+                className="alumni-image"
+              />
+              <Card.Body>
+                <Card.Title>Bob Johnson</Card.Title>
+                <Card.Text>
+                  "I made lifelong friends and learned important skills during
+                  my time here."
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
--- src/components/FETS/FETSPage.js
+++ src/components/FETS/FETSPage.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Image } from "react-bootstrap";
+import { Container, Row, Col, Image, Button } from "react-bootstrap";
 import "./FETS.css";
 
 class FETSPage extends Component {
   render() {
     return (
-      <>
-        <h1>FETS Committee Page</h1>
-      </>
+      <Container className="fets-container">
+        <Row>
+          <Col md={6} className="text-center">
+            <Image
+              src="https://picsum.photos/130/130?image=1027"
+              alt="FETS Logo"
+              fluid
+              className="fets-logo"
+            />
+          </Col>
+          <Col md={6} className="text-center">
+            <h1 className="fets-title">FETS Committee Page</h1>
+            <p className="fets-description">
+              Welcome to the FETS Committee Page. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Aenean varius lorem in semper.
+            </p>
+            <Button variant="primary" className="fets-button">
+              Learn More
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
