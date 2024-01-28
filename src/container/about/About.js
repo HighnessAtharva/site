27029aa0a89b1c3f87d1c1c338709fdcbaeefb11
mathurishani <91481581+mathurishani@users.noreply.github.com>
2024-01-28 12:22:32 +0530

    About content added

--- src/container/about/About.js
+++ src/container/about/About.js
@@ -16,19 +16,35 @@ class About extends Component {
           <Row>
             <Col xs={10} lg={8} md={6} className="mx-auto my-5">
               <h1 className="text-capitalize">
-                about the <br />
-                <strong className="banner-title">University</strong>
+                about  <br />
+                {/* <strong className="banner-title">University</strong> */}
               </h1>
               <p
                 className="my-4 text-muted w-75 about-text"
                 data-aos="fade-right"
               >
-                Sheikh-Ul-Alam Memorial Degree College, Budgam is the first
-                institution of its kind in the district Budgam. The college
-                while upholding the guiding mantra of famous Rishi
-                Sheikh-Ul-Alam (RA) endeavours to bring about social, moral and
-                spiritual transformation of the social set up by bringing it
-                closer to the currents of prevalent knowledge.
+                The B.E. Electronics and Telecommunications Engineering is a
+                comprehensive four-year program that imparts a well-rounded
+                education and a strong foundation in electronics,
+                communications, computing, and engineering sciences. Established
+                in 2005, the department initiated the Electronics &
+                Telecommunication Engineering Course with an intake of 60
+                students and later introduced M.E. (EXTC) in 2010 with 18
+                students. The faculty, equipped with high qualifications and
+                professional expertise, conducts training programs covering
+                Network Administration, Communication, Design & Optimization,
+                Network Security, Embedded Systems, Real-time DSP applications,
+                VLSI design, and RTOS. The department collaborates with
+                companies like Tata Consultancy Services and Sony Ericsson India
+                Ltd through MoUs to enhance students' technical knowledge.
+                Faculty members actively engage in R&D, publishing research in
+                prestigious international journals and conferences such as IEEE,
+                IET, AIP, ASP, Elsevier, etc. Students stay informed through
+                organizations like IEEE, IETE, and F.E.T.S. Graduates find
+                opportunities in communication, networking, and
+                technology-driven fields. Companies such as Sony Ericsson India
+                Ltd, CISCO, Microsoft, IBM, Deloitte, L&T, Tech Mahindra,
+                Infosys, etc., recruit our students on campus.
               </p>
               <a
                 href="/about"
