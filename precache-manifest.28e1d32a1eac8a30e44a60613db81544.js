43b889493a520c471c058a418e492feacb582738
a1749310 <highnessatharva@gmail.com>
2024-02-05 18:41:25 +0530

    Updates

---
+++ precache-manifest.28e1d32a1eac8a30e44a60613db81544.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4994e66685f8b2020b0dd26b1537e08",
+    "url": "/React-University-Website/index.html"
+  },
+  {
+    "revision": "b93113135b0bf7ebbc4e",
+    "url": "/React-University-Website/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "ca83bb01c9dcda41e35d",
+    "url": "/React-University-Website/static/css/main.95b0811e.chunk.css"
+  },
+  {
+    "revision": "b93113135b0bf7ebbc4e",
+    "url": "/React-University-Website/static/js/2.34f85b91.chunk.js"
+  },
+  {
+    "revision": "cfe63c1848a227eaaf4c756b8cf4bf44",
+    "url": "/React-University-Website/static/js/2.34f85b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca83bb01c9dcda41e35d",
+    "url": "/React-University-Website/static/js/main.5d0285ff.chunk.js"
+  },
+  {
+    "revision": "f5cc168e2cb2070e5d4f",
+    "url": "/React-University-Website/static/js/runtime-main.5ce4461a.js"
+  },
+  {
+    "revision": "7c6312efb180c288ff20552bff3499b7",
+    "url": "/React-University-Website/static/media/Book.7c6312ef.mp4"
+  },
+  {
+    "revision": "c42c654c3d81d8f40749fe93d2c1b352",
+    "url": "/React-University-Website/static/media/College Logo.c42c654c.png"
+  },
+  {
+    "revision": "c3b4bda5c02949459f1e040657351fb5",
+    "url": "/React-University-Website/static/media/F1.c3b4bda5.png"
+  },
+  {
+    "revision": "5efbdc82a398201a115d163ce2594604",
+    "url": "/React-University-Website/static/media/F10.5efbdc82.png"
+  },
+  {
+    "revision": "d7760f0285256df4d2f7742a163f0a91",
+    "url": "/React-University-Website/static/media/F11.d7760f02.png"
+  },
+  {
+    "revision": "a80b159cf836b757bc2691fc3f2116a4",
+    "url": "/React-University-Website/static/media/F12.a80b159c.png"
+  },
+  {
+    "revision": "4ab6633b1bdecc24c0da7f935c87b111",
+    "url": "/React-University-Website/static/media/F13.4ab6633b.jpg"
+  },
+  {
+    "revision": "fa946356d7225b6d016db88d6a8075a5",
+    "url": "/React-University-Website/static/media/F14.fa946356.png"
+  },
+  {
+    "revision": "29e23488797133d1db26fce30144fb2d",
+    "url": "/React-University-Website/static/media/F15.29e23488.png"
+  },
+  {
+    "revision": "8fd99ff9f36596cdb784960177b3626c",
+    "url": "/React-University-Website/static/media/F16.8fd99ff9.png"
+  },
+  {
+    "revision": "552f56ca5084c2f9283b0992d4098c5c",
+    "url": "/React-University-Website/static/media/F17.552f56ca.jpg"
+  },
+  {
+    "revision": "3c1d76588a9387c9d98d039fb45849d3",
+    "url": "/React-University-Website/static/media/F18.3c1d7658.jpg"
+  },
+  {
+    "revision": "c0499933391a2d99be155cca99efd0d9",
+    "url": "/React-University-Website/static/media/F19.c0499933.png"
+  },
+  {
+    "revision": "a5229adcc348c0c0ab46753c38c40ea1",
+    "url": "/React-University-Website/static/media/F20.a5229adc.png"
+  },
+  {
+    "revision": "28b690aa89879df35d1685261f9efc61",
+    "url": "/React-University-Website/static/media/F21.28b690aa.jpg"
+  },
+  {
+    "revision": "4e67b52b25df3c10cdbcda8d5ed03e64",
+    "url": "/React-University-Website/static/media/F22.4e67b52b.jpg"
+  },
+  {
+    "revision": "59d294cd2b62c1667c211f85bc10e7f5",
+    "url": "/React-University-Website/static/media/F3.59d294cd.png"
+  },
+  {
+    "revision": "b3ed73af992df0192cd213999e7912b2",
+    "url": "/React-University-Website/static/media/F4.b3ed73af.jpg"
+  },
+  {
+    "revision": "dd6c950acf68a7fe0c92451640fdf273",
+    "url": "/React-University-Website/static/media/F5.dd6c950a.png"
+  },
+  {
+    "revision": "3dadbf0503fd50c11445e374274ce917",
+    "url": "/React-University-Website/static/media/F6.3dadbf05.png"
+  },
+  {
+    "revision": "11d4ed49445f1574bc02fbaeb9726c10",
+    "url": "/React-University-Website/static/media/F7.11d4ed49.jpg"
+  },
+  {
+    "revision": "24cdcd0f70a267c74020082b75af62cc",
+    "url": "/React-University-Website/static/media/F9.24cdcd0f.jpg"
+  },
+  {
+    "revision": "be861a2d12249e63a07b796f22ffb4f2",
+    "url": "/React-University-Website/static/media/SF10.be861a2d.jpg"
+  },
+  {
+    "revision": "7bb2cbed7f77a43755621501bd3d8f48",
+    "url": "/React-University-Website/static/media/SF2.7bb2cbed.jpg"
+  },
+  {
+    "revision": "abfc1402bcb1abc6382dd4f6cd947654",
+    "url": "/React-University-Website/static/media/SF3.abfc1402.jpg"
+  },
+  {
+    "revision": "877d0d29f362382c7f0ed380152aa292",
+    "url": "/React-University-Website/static/media/SF4.877d0d29.jpg"
+  },
+  {
+    "revision": "95026dcb95181ec9c90ea7579a178c8b",
+    "url": "/React-University-Website/static/media/SF5.95026dcb.jpg"
+  },
+  {
+    "revision": "e6e7db762bac89236cdd99240fd70374",
+    "url": "/React-University-Website/static/media/SF6.e6e7db76.jpg"
+  },
+  {
+    "revision": "e0fefae89a25c48a2cd125715f1aebcd",
+    "url": "/React-University-Website/static/media/SF7.e0fefae8.jpg"
+  },
+  {
+    "revision": "596d41eea8edb2b0371fe7674865d12b",
+    "url": "/React-University-Website/static/media/SF8.596d41ee.jpg"
+  },
+  {
+    "revision": "40110c1d52ad1119a07fd38fda53a2a2",
+    "url": "/React-University-Website/static/media/SF9.40110c1d.jpg"
+  },
+  {
+    "revision": "c42c654c3d81d8f40749fe93d2c1b352",
+    "url": "/React-University-Website/static/media/clogo.c42c654c.png"
+  },
+  {
+    "revision": "acdcb857adf691c097f5aa6e0d243791",
+    "url": "/React-University-Website/static/media/course01.acdcb857.jpeg"
+  },
+  {
+    "revision": "c42c654c3d81d8f40749fe93d2c1b352",
+    "url": "/React-University-Website/static/media/logo.c42c654c.png"
+  },
+  {
+    "revision": "19c27dc8b35e90d8b99089f8a393bb70",
+    "url": "/React-University-Website/static/media/slide-01.19c27dc8.jpg"
+  },
+  {
+    "revision": "f50f8336b5a706dab8ca250ee5de37f1",
+    "url": "/React-University-Website/static/media/slide-02.f50f8336.jpg"
+  },
+  {
+    "revision": "347bdbf607b6f2a330fa7e2a5f89d8e5",
+    "url": "/React-University-Website/static/media/slide-03.347bdbf6.jpg"
+  }
+]);
