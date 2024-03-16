bab3fb110d81ef540bd8712c777e8e9c079894de
mathurishani <mathurishani13@gmail.com>
2024-03-16 20:32:43 +0530

    Added Embedded System Lab

--- src/components/Infrastructure/InfrastructurePage.js
+++ src/components/Infrastructure/InfrastructurePage.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
 import "./Infrastructure.css";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 class InfrastructurePage extends Component {
   render() {
     return (
-      <>
+      <Container>
         <h1>Infrastructure</h1>
-
-        <ul>
+        <Card>
+          <Card.Body>
+          <ul>
           <li>
             <Link to="/EmbeddedSystem">Embedded Systems Lab</Link>
           </li>
         </ul>
-      </>
+          </Card.Body>
+        </Card>
+
+        
+      </Container>
     );
   }
 }
