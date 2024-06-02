fbc013f1dd442bef25a20c566b33d7b4ab4c5b2a
a1749310 <highnessatharva@gmail.com>
2024-06-02 16:10:43 +0530

    change root url

--- src/container/header/Topbar.js
+++ src/container/header/Topbar.js
@@ -12,7 +12,7 @@ export class Topbar extends Component {
           className="px-4 text-capitalize d-flex justify-content-center align-items-center"
           style={{ backgroundColor: "white", minHeight: "100px" }}
         >
-          <Navbar.Brand href="/React-University-Website#/">
+          <Navbar.Brand href="/">
             <img src={logo} alt="logo" />
           </Navbar.Brand>
           <Navbar.Text className="ml-3">
